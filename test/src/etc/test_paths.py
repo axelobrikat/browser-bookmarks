42393d271d76a8eb0a2a0a87ce844da7fe2cd7a4
axelobrikat <axel.obrikat@arcor.de>
2023-10-19 22:26:11 +0200

    test_paths.py

--- test/src/etc/test_paths.py
+++ test/src/etc/test_paths.py
@@ -6,3 +6,8 @@ def test_ROOT():
     """test root directory of project, when it is no executable
     """
     assert ROOT == Path(__file__).resolve().parent.parent.parent.parent
+
+def test_repo_name():
+    """test that repo name is last part of root path
+    """
+    assert str(ROOT).split("\\")[-1] == "browser-bookmarks"
