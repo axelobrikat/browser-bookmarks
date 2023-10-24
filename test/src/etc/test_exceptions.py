bea194559b64e697d9eb6b1533466fcbf80e40ea
axelobrikat <axel.obrikat@arcor.de>
2023-10-24 18:13:32 +0200

    docstring test file

--- test/src/etc/test_exceptions.py
+++ test/src/etc/test_exceptions.py
@@ -1,6 +1,8 @@
 from src.etc.exceptions import Exc
 
 def test_format_exc_msg():
+    """test formatting of exception message
+    """
     msg: str = "dummy message"
     assert Exc.format_exc_msg(msg) == (
             f"\n============"
