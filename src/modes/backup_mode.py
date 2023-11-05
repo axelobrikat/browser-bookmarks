5c720db7740b9882344b0bbdf06f882195784f11
axelobrikat <axel.obrikat@arcor.de>
2023-11-05 22:01:21 +0100

    start with backup_mode

--- src/modes/backup_mode.py
+++ src/modes/backup_mode.py
@@ -1,11 +1,23 @@
+from datetime import datetime
+import shutil
+
 from src.modes.mode import Mode
+from src.etc.paths import BOOKMARKS
 
 
 class BackupMode(Mode):
+    """backup bookmarks file
+    - copy, rename and save file to ./data
+    """
     def __init__(self) -> None:
-        pass
+        self.now: datetime|None = None
 
     def process_bookmarks(self):
         """entry function when this mode is selected by user
         """
         pass
+        # save_current_datetime()
+        # transform_date() -> YYMMDD_HHmmss
+        # copy_bookmarks_file() 
+          # with shutil.copyfile(src, dest)
+          # print to cli that copied successful
