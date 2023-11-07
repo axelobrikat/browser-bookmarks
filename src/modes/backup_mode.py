ed1d6a7061ec538439619fee7806334f1a71cf64
Axel Obrikat <axel.obrikat@bmw.de>
2023-11-07 16:17:19 +0100

    backup_mode

--- src/modes/backup_mode.py
+++ src/modes/backup_mode.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 from datetime import datetime
 import shutil
 
 from src.modes.mode import Mode
-from src.etc.paths import BOOKMARKS
+from src.etc.exceptions import Exc
+from src.etc.paths import BOOKMARKS, ROOT
 
 
 class BackupMode(Mode):
@@ -10,7 +12,8 @@ class BackupMode(Mode):
     - copy, rename and save file to ./data
     """
     def __init__(self) -> None:
-        self.now: datetime|None = None
+        self.dest_path: Path = ROOT / "data"
+        self.dest_name: str = ""
 
     def process_bookmarks(self):
         """entry function when this mode is selected by user
@@ -18,7 +21,36 @@ class BackupMode(Mode):
         pass
         # save_current_datetime()
         # transform_date() -> YYMMDD_HHmmss
-          # with f"{str(self.now.year)[-2:]}..."
+            # with f"{str(self.now.year)[-2:]}..."
         # copy_bookmarks_file() 
-          # with shutil.copyfile(src, dest)
-          # print to cli that copied successful
+            # with shutil.copyfile(src, dest)
+            # print to cli that copied successful
+    
+    def copy_bookmarks_file(self):
+        """copy bookmarks file to ./data as file yymmdd_HHMMSS_Backup_Bookmarks
+        - print success message to cli, if successfully copied
+        - otherwise, raise exception and exit program
+        """
+        try:
+            self.dest_name: str = f"{self.get_current_datetime()}_Backup_Bookmarks"
+            shutil.copyfile(
+                BOOKMARKS,
+                self.dest_path / self.dest_name
+            )
+            print(
+                f"Copy of file {BOOKMARKS} successfully saved"
+                f"\n as {self.dest_path / self.dest_name}."
+            )
+        except Exception as e:
+            Exc.exit(
+                f"Cannot create backup of bookmarks file."
+                f"{e}"
+            )
+
+    def get_current_datetime(self) -> str:
+        """return current datetime, format 'yymmdd_HHMMSS')
+
+        Returns:
+            str: current, formatted datetime
+        """
+        return datetime.now().strftime("%y%m%d_%H%M%S")
---
+++ test/src/modes/test_backup_mode.py
@@ -0,0 +1,60 @@
+import pytest
+import os
+from pytest import CaptureFixture
+from pathlib import Path
+from unittest.mock import MagicMock, patch
+from datetime import datetime
+
+from src.modes.backup_mode import BackupMode
+from src.modes import backup_mode
+from src.etc.paths import ROOT
+
+
+def test_init():
+    """test __init__ of class
+    """
+    b = BackupMode()
+    assert b.dest_path == Path(__file__).resolve().parent.parent.parent.parent / "data"
+
+def test_copy_bookmarks_file_success(capsys: CaptureFixture):
+    """test successful copying of bookmarks file and outputtung of success message
+
+    Args:
+        capsys (CaptureFixture): capture cli output
+    """
+    b = BackupMode()
+    mock_bookmarks: Path = Path(ROOT, "test", "testdata", "231030_Bookmarks")
+
+    with patch.object(BackupMode, "get_current_datetime", return_value="dummy"):
+        with patch.object(backup_mode, "BOOKMARKS", mock_bookmarks):
+            b.copy_bookmarks_file()
+
+    test_file: Path = ROOT / "data" / "dummy_Backup_Bookmarks"
+    assert test_file.exists() == True
+    os.remove(test_file)
+
+    assert capsys.readouterr().out == (
+        f"Copy of file {mock_bookmarks} successfully saved"
+        f"\n as {test_file}.\n"
+    )
+
+def test_copy_bookmarks_file_fail():
+    """check that SystemExit Exception gets raised
+    """
+    b = BackupMode()
+
+    with patch.object(backup_mode, "BOOKMARKS", Path("not-existing-path")):
+        with pytest.raises(SystemExit):
+            b.copy_bookmarks_file()
+
+@patch.object(backup_mode, "datetime")
+def test_get_current_datetime(mock_backup_mode: MagicMock|datetime):
+    """test getting of current, correct formatted datetime
+    - mock datetime.now() to return datetime object
+
+    Args:
+        mock_backup_mode (MagicMock | datetime): mocked module datetime
+    """
+    b = BackupMode()
+    mock_backup_mode.now.return_value = datetime(1970, 1, 1, 0, 0, 0)
+    assert b.get_current_datetime() == "700101_000000"
